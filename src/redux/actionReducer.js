9568544d7a88c013e67274ffc1efe15f0bf4f584
Aqib Javed <93072936+ChAqibJavedJaat@users.noreply.github.com>
2022-07-26 09:05:26 -0700

    Add files via upload
    Not Completed Yet...

---
+++ src/redux/actionReducer.js
@@ -0,0 +1,35 @@
+import {
+  GET_USERS_REQUEST,
+  USERS_REQUEST_FAILURE,
+  USERS_REQUEST_SUCCESS,
+} from "./actionTypes";
+
+const initialState = {
+  loading: false,
+  users: {},
+  error: "",
+};
+
+export const userReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case GET_USERS_REQUEST:
+      return {
+        loading: true,
+      };
+    case USERS_REQUEST_SUCCESS:
+      return {
+        ...state,
+        loading: false,
+        users: action.payload,
+        error: "",
+      };
+    case USERS_REQUEST_FAILURE:
+      return {
+        loading: false,
+        users: [],
+        error: action.payload,
+      };
+    default:
+      return state;
+  }
+};
